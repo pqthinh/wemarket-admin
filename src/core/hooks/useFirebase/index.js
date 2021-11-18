e0a1345e2a47bd8d45fef621a31320ac336afe2b
pqthinh <thinhpq@its-global.vn>
2021-11-19 00:00:48 +0700

    config upload file to firebase

--- src/core/hooks/useFirebase/index.js
+++ src/core/hooks/useFirebase/index.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import app from 'firebase'
-import 'firebase/messaging'
+import { storage } from 'config/firebaseConfig.js'
 
-const useFirebase = () => {
-  const [isSupported, setSupported] = React.useState(false)
+const uploadFile = ({ imageAsFile, bucket = '/images/' }) => {
+  var link = ''
+  const uploadTask = storage
+    .ref(`${bucket + imageAsFile.name}`)
+    .put(imageAsFile)
 
-  const firebase = React.useMemo(() => {
-    setSupported(app.messaging.isSupported())
-
-    if (app.apps.length) return app.app()
-
-    return app.initializeApp(process.env.FIREBASE_CONFIG)
-  }, [])
-
-  const messaging = React.useMemo(() => {
-    if (isSupported) return firebase.messaging()
-  }, [isSupported])
-
-  return [messaging]
+  uploadTask.on(
+    'state_changed',
+    snapShot => {
+      //takes a snap shot of the process as it is happening
+      console.log(snapShot)
+    },
+    err => {
+      //catches the errors
+      console.log(err)
+    },
+    () => {
+      // gets the functions from storage refences the image storage in firebase by the children
+      // gets the download url then sets the image from firebase as the value for the imgUrl key:
+      storage
+        .ref('images')
+        .child(imageAsFile.name)
+        .getDownloadURL()
+        .then(fireBaseUrl => {
+          link = fireBaseUrl
+        })
+    }
+  )
+  return link
 }
 
-export default useFirebase
+export { uploadFile }
